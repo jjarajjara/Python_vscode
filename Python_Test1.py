93b141c8e742b73d540553f910806b41a5417280
jjarajjara <156162962+jjarajjara@users.noreply.github.com>
2024-01-23 21:52:47 +0900

    Add files via upload

--- Python_Test1.py
+++ Python_Test1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+
+
 ## NAND , OR 게이트 구현
 # def AND(x1, x2):
 #     x = np.array([x1,x2]) # 입력
@@ -213,70 +215,177 @@ import matplotlib.pylab as plt
 
 ## 3층 신경망 구현하기
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-x))
 
-x = np.array([1.0, 0.5])
-w1 = np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
-b1 = np.array([0.1,0.2,0.3])
+# x = np.array([1.0, 0.5])
+# w1 = np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
+# b1 = np.array([0.1,0.2,0.3])
 
-print(w1.shape)
-print(x.shape)
-print(b1.shape)
+# print(w1.shape)
+# print(x.shape)
+# print(b1.shape)
 
-a1 = np.dot(x,w1) + b1
+# a1 = np.dot(x,w1) + b1
 
-Z1 = sigmoid(a1)
-print(Z1)
-print(a1)
+# Z1 = sigmoid(a1)
+# print(Z1)
+# print(a1)
 
-W2 = np.array([[0.1,0.4], [0.2,0.5], [0.3,0.6]])
-b2 = np.array([0.1,0.2])
+# W2 = np.array([[0.1,0.4], [0.2,0.5], [0.3,0.6]])
+# b2 = np.array([0.1,0.2])
 
-print(Z1.shape)
-print(W2.shape)
-print(b2.shape)
+# print(Z1.shape)
+# print(W2.shape)
+# print(b2.shape)
 
-a2 = np.dot(Z1,W2) + b2
-Z2 = sigmoid(a2)
+# a2 = np.dot(Z1,W2) + b2
+# Z2 = sigmoid(a2)
 
-def identity_function(x):
-    return x
+# def identity_function(x):
+#     return x
 
-W3 = np.array([[0.1,0.3], [0.2,0.4]])
-b3 = np.array([0.1,0.2])
+# W3 = np.array([[0.1,0.3], [0.2,0.4]])
+# b3 = np.array([0.1,0.2])
 
-a3 = np.dot(Z2,W3) + b3
-Y = identity_function(a3)
+# a3 = np.dot(Z2,W3) + b3
+# Y = identity_function(a3)
 
 
-## 구현 정리
+# ## 구현 정리
+# ## init_network() : 가중치와 편향을 초기화하고 딕셔너리 변수 network에 저장한다
+# ## 딕셔너리 변수 network에는 각 층에 필요한 매개변수(가중치, 편향)을 저장한다
 
-def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
-    network['b1'] = np.array([0.1,0.2,0.3])
-    network['W2'] = np.array([[0.1,0.4], [0.2,0.5], [0.3,0.6]])
-    network['b2'] = np.array([0.1,0.2])
-    network['W3'] = np.array([[0.1,0.3], [0.2,0.4]])
-    network['b3'] = np.array([0.1,0.2])
+# def init_network():
+#     network = {}
+#     network['W1'] = np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
+#     network['b1'] = np.array([0.1,0.2,0.3])
+#     network['W2'] = np.array([[0.1,0.4], [0.2,0.5], [0.3,0.6]])
+#     network['b2'] = np.array([0.1,0.2])
+#     network['W3'] = np.array([[0.1,0.3], [0.2,0.4]])
+#     network['b3'] = np.array([0.1,0.2])
 
-    return network
+#     return network
 
-def forward(network, x):
-    W1, W2, W3 = network['W1'],network['W2'],network['W3']
-    b1, b2, b3 = network['b1'],network['b2'],network['b3']
+# ## forward() : 입력 신호를 출력으로 변환하는 처리 과정을 모두 구현한다
+# ## 입력 신호가 순방향(입력에서 출력 방향)으로 전달됨에 주의하자
 
-    a1 = np.dot(x,W1) + b1
-    Z1 = sigmoid(a1)
-    a2 = np.dot(Z1,W2) + b2
-    Z2 = sigmoid(a2)
-    a3 = np.dot(Z2,W3) + b3
-    Y = identity_function(a3)
+# def forward(network, x):
+#     W1, W2, W3 = network['W1'],network['W2'],network['W3']
+#     b1, b2, b3 = network['b1'],network['b2'],network['b3']
 
-    return Y
+#     a1 = np.dot(x,W1) + b1
+#     Z1 = sigmoid(a1)
+#     a2 = np.dot(Z1,W2) + b2
+#     Z2 = sigmoid(a2)
+#     a3 = np.dot(Z2,W3) + b3
+#     Y = identity_function(a3)
 
-network = init_network()
-x = np.array([1.0,0.5])
-y = forward(network, x)
-print(y)
+#     return Y
+
+# network = init_network()
+# x = np.array([1.0,0.5])
+# y = forward(network, x)
+# print(y)
+
+## 출력층 설계하기
+## 일반적으로 회귀에는 항등 함수를, 분류에는 소프트맥스 함수를 사용한다
+## 항등 함수 : 입력을 그대로 출력한다
+## 소프트맥스 함수 : 입력 신호를 정규화하여 출력한다
+
+## 기계학습 문제는 분류(classification)와 회귀(regression)로 나눌 수 있다
+## 분류(classification) : 데이터가 어느 클래스에 속하느냐 문제
+## 회귀(regression) : 입력 데이터에서 (연속적인) 수치를 예측하는 문제
+
+## 항등 함수(identity function) : 입력을 그대로 출력하는 함수
+## 출력층에서 항등 함수를 사용하면 입력 신호가 그대로 출력 신호가 된다
+
+## 소프트맥스 함수(softmax function) : 입력 값을 정규화하여 출력한다
+
+## yk = exp(ak) / sigma(i=1~n) exp(ai)
+
+## n : 출력층의 뉴런 수
+## yk : k번째 출력
+## ak : k번째 출력의 입력 신호
+
+
+## 소프트맥스 함수 구현하기
+
+# a = np.array([0.3,2.9,4.0])
+
+# exp_a = np.exp(a) ## 지수 함수
+# print(exp_a)
+
+# sum_exp_a = np.sum(exp_a) ## 지수 함수의 합
+# print(sum_exp_a)
+
+# y = exp_a / sum_exp_a
+# print(y)
+
+# ## exp_a 함수란? : 밑(base)이 자연상수 e인 지수 함수
+
+
+# def softmax(a):
+#     exp_a = np.exp(a)
+#     sum_exp_a = np.sum(exp_a)
+#     y = exp_a / sum_exp_a
+
+#     return y
+
+# a = np.array([1010,1000,990])
+# print(np.exp(a) / np.sum(np.exp(a)))
+
+ ## 소프트맥스 함수의 계산 결과는 모두 0이 되어버린다
+## nan : not a number
+## 해결책 : 입력 신호 중 최댓값을 빼주면 올바르게 계산할 수 있다
+
+# c = np.max(a)  ## c = 1010
+# print(a-c)
+
+# print(np.exp(a-c) / np.sum(np.exp(a-c)))
+
+## 소프트맥스 함수 구현 정리
+## 1. 입력 신호 중 최댓값을 빼준다(오버플로 대책)
+## 2. exp() 함수를 적용한다
+## 3. exp() 함수의 출력을 모두 더한다
+## 4. 3의 결과로 나온 값을 분모와 분자로 나눈다
+
+# def softmax(a):
+#     c = np.max(a)
+#     exp_a = np.exp(a-c) ## 오버플로 대책
+#     sum_exp_a = np.sum(exp_a)
+#     y = exp_a / sum_exp_a
+
+#     return y
+
+# ## 소프트맥스 함수의 특징
+# ## 소프트맥스 함수의 출력은 0에서 1.0 사이의 실수이다
+
+# a = np.array([0.3,2.9,4.0])
+# y = softmax(a)
+# print(y)
+# print(np.sum(y)) ## 소프트맥스 함수의 출력은 0에서 1.0 사이의 실수이다
+
+## 소프트맥스 함수의 출력 총합은 1이다
+##  -> 이 성질 덕분에 소프트맥스 함수의 출력을 '확률'로 해석할 수 있다
+
+## 기계 학습의 문제 풀이 학습과 추론의 두 단계
+## 학습 : 모델을 학습하하는 것 -> 훈련 데이터를 사용하여 가중치 매개변수를 학습하는 것
+## 추론 : 앞서 학습한 모델로 미지의 데이터에 대해서 추론(분류)하는 것
+
+## 신경망에서는 학습 때는 Softmax 함수를 사용하고, 
+## 추론 때는 Softmax 함수를 생략하는 것이 일반적이다
+
+import sys, os
+sys.path.append(os.pardir) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+from dataset.mnist import load_mnist
+from PIL import Image
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+## 각 데이터의 형상 출력
+print(x_train.shape) 
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
