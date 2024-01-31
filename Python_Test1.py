75a2dbf3c5fddcfd5335906c9d30dc7fa48ab00d
jjarajjara <156162962+jjarajjara@users.noreply.github.com>
2024-01-31 22:56:28 +0900

    Add files via upload

--- Python_Test1.py
+++ Python_Test1.py
@@ -542,18 +542,18 @@ import numpy as np
 
 ## 오차제곱합 구현  
 
-def sum_squares_error(y, t):
-    return 0.5 * np.sum((y-t)**2)
+# def sum_squares_error(y, t):
+#     return 0.5 * np.sum((y-t)**2)
 
-t = [0,0,1,0,0,0,0,0,0,0] ## 정답은 '2'
+# t = [0,0,1,0,0,0,0,0,0,0] ## 정답은 '2'
 
-y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0] ## '2'일 확률이 가장 높다고 추정함
+# y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0] ## '2'일 확률이 가장 높다고 추정함
 
-print(sum_squares_error(np.array(y), np.array(t))) ## 0.09750000000000003
+# print(sum_squares_error(np.array(y), np.array(t))) ## 0.09750000000000003
 
-y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0] ## '7'일 확률이 가장 높다고 추정함
+# y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0] ## '7'일 확률이 가장 높다고 추정함
 
-print(sum_squares_error(np.array(y), np.array(t))) ## 0.5975
+# print(sum_squares_error(np.array(y), np.array(t))) ## 0.5975
 
 ## 교차 엔트로피 오차(cross entropy error, CEE)
 
@@ -564,14 +564,105 @@ print(sum_squares_error(np.array(y), np.array(t))) ## 0.5975
 
 ## 엔트로피 오차 구현 
 
-def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta)) ## delta : 아주 작은 값(0.0000001) -> np.log() 함수에 0을 입력하면 마이너스 무한대를 뜻하는 -inf가 되어 더 이상 계산을 진행할 수 없다
+# def cross_entropy_error(y, t):
+#     delta = 1e-7
+#     return -np.sum(t * np.log(y + delta)) ## delta : 아주 작은 값(0.0000001) -> np.log() 함수에 0을 입력하면 마이너스 무한대를 뜻하는 -inf가 되어 더 이상 계산을 진행할 수 없다
 
-t = [0,0,1,0,0,0,0,0,0,0] ## 정답은 '2'
-y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0] ## '2'일 확률이 가장 높다고 추정함
-print(cross_entropy_error(np.array(y), np.array(t))) ## 0.510825457099338
+# t = [0,0,1,0,0,0,0,0,0,0] ## 정답은 '2'
+# y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0] ## '2'일 확률이 가장 높다고 추정함
+# print(cross_entropy_error(np.array(y), np.array(t))) ## 0.510825457099338
 
-y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0] ## '7'일 확률이 가장 높다고 추정함
-print(cross_entropy_error(np.array(y), np.array(t))) ## 2.302584092994546
+# y = [0.1,0.05,0.1,0.0,0.05,0.1,0.0,0.6,0.0,0.0] ## '7'일 확률이 가장 높다고 추정함
+# print(cross_entropy_error(np.array(y), np.array(t))) ## 2.302584092994546
+
+## E = -1/N sigma(k=1~n) sigma(n=1~N) tnk*logynk
+## N : 데이터의 수  -> 데이터 하나당 오차를 평균낸 것
+## tnk : n번째 데이터의 k번째 값을 의미
+## ynk : 신경망의 출력
+## log : 밑이 e인 자연로그
+
+## 미니배치
+## 훈련 데이터로부터 일부만 골라 학습을 수행 
+
+## 미니배치 학습 구현
+
+# import sys, os
+# sys.path.append(os.pardir) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+# import numpy as np
+# from dataset.mnist import load_mnist
+
+# (x_train, t_train), (x_test, t_test) = \
+#     load_mnist(normalize=True, one_hot_label=True) 
+
+# ## normalize=True : 입력 이미지의 픽셀 값을 0.0 ~ 1.0 사이의 값으로 정규화한다
+# ## one_hot_label=True : 원-핫 인코딩 형태로 저장한다
+
+# print(x_train.shape) ## (60000, 784)
+# print(t_train.shape) ## (60000, 10)
+
+# ## np.random.choice() 함수 : 지정한 범위의 수 중에서 무작위로 원하는 개수만 꺼낼 수 있다
+
+# train_size = x_train.shape[0]
+# batch_size = 10
+# batch_mask = np.random.choice(train_size, batch_size) ## 0~59999 사이의 수 중에서 무작위로 10개를 골라낸다
+# x_batch = x_train[batch_mask]
+# t_batch = t_train[batch_mask]
+
+# print(batch_mask)
+
+## 수치 미분의 예 
+
+# import numpy as np
+# import matplotlib.pylab as plt
+
+# def numerical_diff(f, x):
+#     h = 1e-4
+#     return (f(x+h) - f(x-h)) / (2*h) 
+# ## 중심 차분(중앙 차분) : x+h와 x-h일 때의 함수 f의 차분을 계산
+
+# def function_1(x):
+#     return 0.01*x**2 + 0.1*x ## y = 0.01x^2 + 0.1x
+
+# x = np.arange(0.0, 20.0, 0.1) ## 0에서 20까지 0.1 간격의 배열 x를 만든다
+# y = function_1(x)
+
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+# plt.plot(x,y)
+# plt.show()
+
+# print(numerical_diff(function_1, 5)) ## 0.1999999999990898
+# print(numerical_diff(function_1, 10)) ## 0.2999999999986347
+
+import numpy as np
+import matplotlib.pylab as plt
+
+## 편미분 
+def function_2(x):
+    return x[0]**2 + x[1]**2 ## 또는 np.sum(x**2)
+
+## 기울기 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x) ## x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        ## f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        ## f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1-fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+print(numerical_gradient(function_2, np.array([3.0,4.0]))) ## [6. 8.]
+print(numerical_gradient(function_2, np.array([0.0,2.0]))) ## [0. 4.]
+print(numerical_gradient(function_2, np.array([3.0,0.0]))) ## [6. 0.]
+
 
--- functions.py
+++ functions.py
@@ -1,6 +1,30 @@
 # coding: utf-8
 import numpy as np
 
+def numercial_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+## 1e-4는 0.0001을 의미한다
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+        
+    return grad
 
 def identity_function(x):
     return x
@@ -49,6 +73,17 @@ def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+## y = 신경망의 출력 
+## t = 정답 레이블
+
+## 원-핫 인코딩일 때
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
         
     # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
     if t.size == y.size:
@@ -56,6 +91,10 @@ def cross_entropy_error(y, t):
              
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+## np.arange(batch_size)는 0부터 batch_size - 1까지 배열을 생성한다
+
+## 정답에 해당하는 신경망의 출력만으로 교차 엔트로피 오차를 구할 수 있다 
+
 
 
 def softmax_loss(X, t):
