051cef88ff24eb3d712e63ebad55d5fb86054293
jjarajjara <156162962+jjarajjara@users.noreply.github.com>
2024-02-07 15:07:18 +0900

    Add files via upload

--- Python_Test1.py
+++ Python_Test1.py
@@ -883,49 +883,59 @@
 
 ## 에폭(epoch) : 하나의 단위로, 학습에서 훈련 데이터를 모두 소진했을 때의 횟수를 의미한다
 
-import numpy as np
-from dataset.mnist import load_mnist
-from two_layer_net import TwoLayerNet
+# import numpy as np
+# from dataset.mnist import load_mnist
+# from two_layer_net import TwoLayerNet
 
-(x_train, t_train), (x_test, t_test) = \
-    load_mnist(normalize=True, one_hot_label=True)
+# (x_train, t_train), (x_test, t_test) = \
+#     load_mnist(normalize=True, one_hot_label=True)
     
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-iters_num = 10000
-train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.1
+# iters_num = 10000
+# train_size = x_train.shape[0]
+# batch_size = 100
+# learning_rate = 0.1
 
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
+# train_loss_list = []
+# train_acc_list = []
+# test_acc_list = []
 
-## 1에폭당 반복 수
-iter_per_epoch = max(train_size / batch_size, 1)
+# ## 1에폭당 반복 수
+# iter_per_epoch = max(train_size / batch_size, 1)
 
-for i in range(iters_num):
-    ## 미니배치 획득
-    batch_mask = np.random.choice(train_size, batch_size)
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
+# for i in range(iters_num):
+#     ## 미니배치 획득
+#     batch_mask = np.random.choice(train_size, batch_size)
+#     x_batch = x_train[batch_mask]
+#     t_batch = t_train[batch_mask]
 
-    ## 기울기 계산
-    grad = network.numercial_gradient(x_batch, t_batch)
-    ## grad = network.gradient(x_batch, t_batch) ## 성능 개선판
+#     ## 기울기 계산
+#     grad = network.numercial_gradient(x_batch, t_batch)
+#     ## grad = network.gradient(x_batch, t_batch) ## 성능 개선판
 
-    ## 매개변수 갱신
-    for key in ('W1', 'b1', 'W2', 'b2'):
-        network.params[key] -= learning_rate * grad[key]
+#     ## 매개변수 갱신
+#     for key in ('W1', 'b1', 'W2', 'b2'):
+#         network.params[key] -= learning_rate * grad[key]
 
-    ## 학습 경과 기록
-    loss = network.loss(x_batch, t_batch)
-    train_loss_list.append(loss)
+#     ## 학습 경과 기록
+#     loss = network.loss(x_batch, t_batch)
+#     train_loss_list.append(loss)
 
-    ## 1에폭당 정확도 계산
-    if i % iter_per_epoch == 0:
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+#     ## 1에폭당 정확도 계산
+#     if i % iter_per_epoch == 0:
+#         train_acc = network.accuracy(x_train, t_train)
+#         test_acc = network.accuracy(x_test, t_test)
+#         train_acc_list.append(train_acc)
+#         test_acc_list.append(test_acc)
+#         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+## 오차역전파법 ( 가중치 매개변수의 기울기를 효율적으로 계산 )
+
+## 1. 수식을 통한 것 ( 일반적인 방법 )
+## 2. 계산 그래프를 통한 것 
+
+## 계산 그래프 (computational graph) : 계산 과정을 그래프로 나타낸 것
+
+
